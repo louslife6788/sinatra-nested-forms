4b82135cd7871bdb078a0cfd3bffb3149d851e0b
luis leon <leonluis380@yahoo.com>
2022-01-09 23:13:54 -0500

    commit tree f6a99a80877fa7dbc5eba3d80abb9dc6f0860b8b

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,25 @@
 class Ship
+
+    attr_reader :name, :type, :booty
+
+    ALL = []
+  
+    def initialize(args)
+      @name = args[:name]
+      @type = args[:type]
+      @booty = args[:booty]
+      ALL << self
+    end
+  
+    def self.all
+      ALL
+    end
+
+    def self.clear
+
+        ALL.clear
+    
+    end
+
+
 end
